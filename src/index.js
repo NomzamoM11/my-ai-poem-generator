1b5a598c8e05a3f94d147bd7ace4d25c0917e710
NomzamoM11 <mnomzamo83@gmail.com>
2024-12-04 09:07:32 +0200

    Added the AI API immigration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "In the tapestry of life, they stand adorned",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "4507d3fa7d70aoadbteec32b558d1f39";
+  let prompt = `User instructions: Generate a short poem about ${instructionInput.value}, ensure to strictly follow the user instructions`;
+  let context =
+    "You are a creative poem writer, with a talent for short poems. Your task is to generate a 4 line poem using basic HTML, separating each line with <br />. The poem should be signed 'SheCodes AI' inside a <strong> element at the end, Strictly NO title at the beginning of the poem";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
